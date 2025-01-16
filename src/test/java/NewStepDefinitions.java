92c4544c19a60677afd7bdc3e4e974f1f61078db
bandagaurav <42979145+bandagaurav@users.noreply.github.com>
2025-01-16 12:37:13 +0530

    Create src/test/java/NewStepDefinitions.java

---
+++ src/test/java/NewStepDefinitions.java
@@ -0,0 +1,16 @@
+public class NewStepDefinitions {
+    @Given("some initial context")
+    public void someInitialContext() {
+        // implementation
+    }
+
+    @When("an action is performed")
+    public void anActionIsPerformed() {
+        // implementation
+    }
+
+    @Then("expect some result")
+    public void expectSomeResult() {
+        // implementation
+    }
+}
