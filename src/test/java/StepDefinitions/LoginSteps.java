17d140ef525e07ca58a5541f32b906d0569aa496
bandagaurav <42979145+bandagaurav@users.noreply.github.com>
2025-02-25 19:19:44 +0530

    Create src/test/java/StepDefinitions/LoginSteps.java

---
+++ src/test/java/StepDefinitions/LoginSteps.java
@@ -0,0 +1,53 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+public class LoginSteps {
+
+    @Given("the user is on the login page")
+    public void the_user_is_on_the_login_page() {
+        // Code to navigate to login page
+    }
+
+    @When("the user enters the username {string} and password {string}")
+    public void the_user_enters_the_username_and_password(String username, String password) {
+        // Code to enter username and password
+    }
+
+    @When("clicks the login button")
+    public void clicks_the_login_button() {
+        // Code to click login button
+    }
+
+    @Then("the user should be successfully logged in")
+    public void the_user_should_be_successfully_logged_in() {
+        // Code to verify successful login
+    }
+
+    @Then("the user should have access to the private area")
+    public void the_user_should_have_access_to_the_private_area() {
+        // Code to verify access to private area
+    }
+
+    @Then("the user should not be able to access the private area")
+    public void the_user_should_not_be_able_to_access_the_private_area() {
+        // Code to verify no access to private area
+    }
+
+    @Then("the user should be notified with an appropriate error message")
+    public void the_user_should_be_notified_with_an_appropriate_error_message() {
+        // Code to verify error message
+    }
+
+    @Then("the login process should be secure")
+    public void the_login_process_should_be_secure() {
+        // Code to verify secure login process
+    }
+
+    @Then("user information should be protected following best practices")
+    public void user_information_should_be_protected_following_best_practices() {
+        // Code to verify user information protection
+    }
+}
