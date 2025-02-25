7fde65795f7aafe3f9c59af3d8702a597ff6382c
bandagaurav <42979145+bandagaurav@users.noreply.github.com>
2025-02-25 13:26:49 +0530

    Create src/test/java/StepDefinitions/GAUR223StepDefinitions.java

---
+++ src/test/java/StepDefinitions/GAUR223StepDefinitions.java
@@ -0,0 +1,23 @@
+package StepDefinitions;
+
+import io.cucumber.java.en.Given;
+import io.cucumber.java.en.When;
+import io.cucumber.java.en.Then;
+
+public class GAUR223StepDefinitions {
+
+    @Given("the application is launched")
+    public void the_application_is_launched() {
+        // Code to launch the application
+    }
+
+    @When("the user performs action A")
+    public void the_user_performs_action_A() {
+        // Code for action A
+    }
+
+    @Then("the result should be B")
+    public void the_result_should_be_B() {
+        // Code to verify result B
+    }
+}
