bb7119463ede42a04ff2043bc313f778cfd92ac6
bandagaurav <42979145+bandagaurav@users.noreply.github.com>
2025-02-28 18:41:05 +0530

    Create src/test/java/pages/LoginPage_v1.java

---
+++ src/test/java/pages/LoginPage_v1.java
@@ -0,0 +1,16 @@
+public class LoginPage {
+
+    // Existing methods...
+
+    public String getErrorMessage() {
+        // Code to retrieve the error message from the login page
+    }
+
+    public boolean isLoginProcessSecure() {
+        // Code to verify if the login process is secure
+    }
+
+    public boolean isUserInformationProtected() {
+        // Code to verify if the user information is protected
+    }
+}
